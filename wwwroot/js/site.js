fe12ca94d270bf782d595194e85b715078ada9d7
Ammar-AlKeemyawee <Ammar.AlKeemyawee@my-aolcc.com>
2025-08-13 18:50:11 +0300

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.getElementById("animated-header");
+    let hue = 0;
+
+    function animateHeader() {
+        hue = (hue + 1) % 360;
+        header.style.color = `hsl(${hue}, 80%, 60%)`;
+        requestAnimationFrame(animateHeader);
+    }
+
+    animateHeader();
+});
